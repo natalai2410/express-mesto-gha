dcab2308b2879c64ab1111a2e29c508c16c6361e
natkru <n9215841072@gmail.com>
2022-09-15 21:22:27 +0300

    ++

--- controllers/users.js
+++ controllers/users.js
@@ -81,34 +81,23 @@ const updateAvatar = (req, res, next) => {
 // POST /users — создаёт пользователя
 const createUser = (req, res, next) => {
   const {
-    name, about, avatar, _id, email, password,
+    name, about, avatar, email,
   } = req.body;
-  bcrypt.hash(password, 10)
+  bcrypt.hash(req.body.password, 10)
     .then((hash) => {
-      const user = new User({
-        name,
-        about,
-        avatar,
-        _id,
-        email,
-        password: hash,
-      });
       User.create({
         name, about, avatar, email, password: hash,
-      })
-        .then(() => {
-          res.send(user);
-        // eslint-disable-next-line consistent-return
-        }).catch((err) => {
-          if (err.code === 11000) {
-            return next(new ConflictError('Пользователь с таким email уже существует'));
-          }
-          if ((err.kind === 'ObjectId') || (err.name === 'ValidationError')) {
-            return next(new ValidationError('Некорректный запрос'));
-          }
-        });
+      });
     })
-    .catch(() => next(new ServerError('Произошла ошибка')));
+    .then((user) => res.status(201).send(user))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        throw new ValidationError('Переданы некорректные данные при создании пользователя');
+      } else if (err.code === 11000) {
+        throw new ConflictError('Пользователь с таким email уже существует');
+      }
+    })
+    .catch(next);
 };
 
 const login = (req, res, next) => {
