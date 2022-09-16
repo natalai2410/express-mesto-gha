2489598ef9f61e1237be6452a50e5bb092449e4c
natkru <n9215841072@gmail.com>
2022-09-16 11:20:00 +0300

    56

--- controllers/users.js
+++ controllers/users.js
@@ -73,7 +73,27 @@ const updateAvatar = (req, res, next) => {
     });
 };
 
-// POST /users — создаёт пользователя
+// // POST /users — создаёт пользователя
+// const createUser = (req, res, next) => {
+//   const {
+//     name,
+//     about,
+//     avatar,
+//     email,
+//   } = req.body;
+//   bcrypt.hash(req.body.password, 10).then((hash) => User.create({
+//     name, about, avatar, email, password: hash,
+//   }))
+//     .then(() => res.send(name, about, avatar, email))
+//     .catch((err) => {
+//       if (err.name === 'ValidationError') {
+//         next(new ValidationError('Переданы некорректные данные при создании пользователя'));
+//       } else if (err.code === 11000) {
+//         next(new ConflictError('Пользователь с таким email уже существует'));
+//       } else { next(err); }
+//     });
+// };
+
 const createUser = (req, res, next) => {
   const {
     name,
@@ -81,17 +101,20 @@ const createUser = (req, res, next) => {
     avatar,
     email,
   } = req.body;
-  bcrypt.hash(req.body.password, 10).then((hash) => User.create({
-    name, about, avatar, email, password: hash,
-  }))
+  bcrypt.hash(req.body.password, 10).then((hash) => {
+    User.create({
+      name, about, avatar, email, password: hash,
+    });
+  })
     .then(() => res.send(name, about, avatar, email))
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        next(new ValidationError('Переданы некорректные данные при создании пользователя'));
+        throw new ValidationError('Переданы некорректные данные при создании пользователя');
       } else if (err.code === 11000) {
-        next(new ConflictError('Пользователь с таким email уже существует'));
-      } else { next(err); }
-    });
+        throw new ConflictError('Пользователь с таким email уже существует');
+      }
+    })
+    .catch(next);
 };
 
 const login = (req, res, next) => {
